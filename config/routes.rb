455b8bd727ee7cd8f365fc137c8a2966f5632f9e
Paulo Gomes <paulovianagomes@gmail.com>
2015-02-13 12:12:26 -0200

    root route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
     collection { post :import }
   end
 
+  root "roadways#new"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
