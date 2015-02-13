74ec886bdbf151073343d77ec7a3d44cecae19d5
Paulo Gomes <paulovianagomes@gmail.com>
2015-02-12 22:08:31 -0200

    Add Test

--- test/models/roadway_test.rb
+++ test/models/roadway_test.rb
@@ -4,4 +4,60 @@ class RoadwayTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not save Roadway empty" do
+    roadway = Roadway.new
+    assert_not roadway.save
+  end
+
+  test "should not save Roadway without tipo" do
+    roadway = Roadway.new
+    roadway.rodovia = 'BR-1'
+    roadway.sentido = 'norte'
+    roadway.conservacao = '5'
+    assert_not roadway.save
+  end
+
+  test "should not save Roadway without rodovia" do
+    roadway = Roadway.new
+    roadway.tipo = 'acesso'
+    roadway.sentido = 'norte'
+    roadway.conservacao = '5'
+    assert_not roadway.save
+  end
+
+  test "should not save Roadway without sentido" do
+    roadway = Roadway.new
+    roadway.tipo = 'acesso'
+    roadway.rodovia = 'BR-1'
+    roadway.conservacao = '5'
+    assert_not roadway.save
+  end
+
+  test "should not save Roadway without conservacao" do
+    roadway = Roadway.new
+    roadway.tipo = 'acesso'
+    roadway.rodovia = 'BR-1'
+    roadway.sentido = 'norte'
+    assert_not roadway.save
+  end
+
+  test "should not save Roadway with wrong sentido" do
+    roadway = Roadway.new
+    roadway.tipo = 'acesso'
+    roadway.rodovia = 'BR-1'
+    roadway.sentido = 'pra la' #
+    roadway.conservacao = '5'
+    assert_not roadway.save
+  end
+
+  test "should not save Roadway with wrong conservacao" do
+    roadway = Roadway.new
+    roadway.tipo = 'acesso'
+    roadway.rodovia = 'BR-1'
+    roadway.sentido = 'pra la' #
+    roadway.conservacao = 'A'
+    assert_not roadway.save
+  end
+
 end
